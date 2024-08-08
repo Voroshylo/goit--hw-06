6abc66274d555300fc610a64d3b20544fb006f60
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-08-08 12:51:33 +0300

    fix component

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { store, persistor } from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <persistGate loading={null} persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </persistGate>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
